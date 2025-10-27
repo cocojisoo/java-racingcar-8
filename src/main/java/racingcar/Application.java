c3819b1e6e3066be2d01dc8f5d8fb78bbf0a97c2
cocojisoo <yjs1019a@gmail.com>
2025-10-27 20:23:48 +0900

    feat(model): add Car model

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,34 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.service.ParseInput;
+import racingcar.service.Race;
+import racingcar.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ParseInput parseInput = new ParseInput();
+        
+        String carNamesInput = inputView.getCarNames();
+        String attemptsInput = inputView.getAttempts();
+        
+        List<String> carNames = parseInput.parseCarNames(carNamesInput);
+        int attempts = Integer.parseInt(attemptsInput);
+        
+        Race race = new Race(carNames);
+        
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < attempts; i++) {
+            race.runRound();
+            race.displayRoundResult();
+        }
+        
+        displayWinners(race);
+    }
+
+    private static void displayWinners(Race race) {
+        List<String> winners = race.getWinners();
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
     }
 }
